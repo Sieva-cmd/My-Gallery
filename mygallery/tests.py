60a6bba04700a290bb8fac4c5960953210030c23
Sieva-cmd <musyokasieva@gmail.com>
2022-03-27 18:57:35 +0300

    add tests

--- mygallery/tests.py
+++ mygallery/tests.py
@@ -14,9 +14,37 @@ class ImageTestClass(TestCase):
         self.web.save_image()  
         images =Image.objects.all()
         self.assertTrue(len(images) > 0)  
-    def test_delete_image(self):
-         self.web.save_image() 
-         self.assertTrue(Image.delete_image(1),True)
+    # def test_delete_image(self):
+    #      self.web.save_image() 
+    #      self.assertTrue(Image.delete_image(1),True)
+
+    class CategoryTestClass(TestCase):
+       def setUp(self):
+        self.category = Category(category='Fashion')
+
+       def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+    
+    #Test to save the method
+       def test_save_method(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+    
+
+
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(location="Nairobi")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)     
 
    
      
