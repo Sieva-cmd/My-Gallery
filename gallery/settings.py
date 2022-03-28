596416e0060f4f512e6681d778ab907501c526a6
Sieva-cmd <musyokasieva@gmail.com>
2022-03-28 11:23:02 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -55,12 +55,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c#-^0!7kfd)36l7dpa#(k_7%kn2yxrnmj28p#fj5a!ibq5g8v!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -113,14 +111,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'photos',
-#         'USER': 'moringa',
-#         'PASSWORD':'sieva',
-#     }
-# }
+
 
 
 # Password validation
